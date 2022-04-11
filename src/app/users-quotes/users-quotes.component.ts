ac310c8eb8cae83051c807e3453b956367e16a07
John Muiruri <themasalacollective@gmail.com>
2022-04-11 05:24:04 +0300

    Adds user quotes component

--- src/app/users-quotes/users-quotes.component.ts
+++ src/app/users-quotes/users-quotes.component.ts
@@ -9,6 +9,15 @@ export class UsersQuotesComponent implements OnInit {
 
   constructor() { }
 
+  quote: any = {
+    quoteId:"1",
+    quoteTxt:"opportunity may knock once but temptation leans on the doorbell",
+    author:"Mr Inspirational", 
+    likes: 0, 
+    dislikes: 0, 
+    publishDate: "10-04-22"
+  }
+  
   ngOnInit(): void {
   }
 
